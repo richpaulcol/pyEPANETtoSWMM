44eadeb4bf3a6f664fd5b6ea840b2da7a0a0ef7d
Richard Collins <r.p.collins@sheffield.ac.uk>
2020-05-15 19:23:27 +0100

    A basic implementation.
    Not yet implemented
    Pump curves
    Patterns (fully)
    Controls

--- inpClasses.py
+++ inpClasses.py
@@ -1,13 +1,82 @@
+class Network:
+    def __init__(self,Name,Title=[],Juncs=[],Reservoirs=[],Tanks=[],Pipes=[],Pumps=[],Valves=[],Tags=[],Demands=[],Patterns=[],Curves=[],Controls=[],Emitters=[],Coordinates=[],Vertices=[],Labels=[],Options=[]):
+        self.name = Name
+        self.titles = Title
+        self.juncs = Juncs
+        self.reservoirs = Reservoirs
+        self.tanks = Tanks
+        self.pipes = Pipes
+        self.pumps = Pumps
+        self.valves = Valves
+        self.tags = Tags
+        self.demands = Demands
+        self.patterns = Patterns
+        self.curves = Curves
+        self.controls = Controls
+        self.emitters = Emitters
+        self.coordinates = Coordinates
+        self.labels = Labels
+        self.options = Options
+        self.vertices = Vertices
+
+    #def SWMM_file(self,filename):
+    #    self.filename = filename
+    #    f = open(self.filename,w)
+
+
+
 class Junc:
+    """
+    This is the junction class, it allows you to input data from a EPANET format and has functions for writing to a SWMM format.
+    """
     def __init__(self,id,elevation,demand,pattern = None):
         self.id = id
-        self.elev = float(elevation)
+        self.elev = elevation
         self.demand = demand
+        self.pattern = pattern
+
+    def SWMM_writer(self,max_depth = 1000,init_depth = 0,surcharge_depth = 0,ponded_area=0):
+        """
+        This function will produce a list formatted in the order required by SWMM inp files.
+        It uses junction ids and elevations previously read into the class and the following additional optional inputs:
+        :param max_depth:   If not specified will default to
+        :param init_depth:  If not specified will default to 0
+        :param surcharge_depth:  If not specified will default to 0
+        :param ponded_area:  If not specified will default to 0
+        :return list formatted in SWMM junction format:
+        """
+        junction = [self.id,str(self.elev),str(max_depth),str(init_depth),str(surcharge_depth),str(ponded_area)]
+        return junction
+
+    def SWMM_demand_writer(self,Constit='FLOW', timeSeries='""', pattType='FLOW', mFactor=1.0, sFactor=1.0):
+        demand = [str(self.id), str(Constit), str(timeSeries), str(pattType), str(mFactor), str(sFactor),
+                      str(-float(self.demand)), str(self.pattern)]
+        return demand
+
+class Title:
+    def __init__(self,text):
+        self.text = text
 
 class Reservoir:
     def __init__(self, id, head):
         self.id = id
         self.head = head
+
+    def SWMM_writer(self,maxDepth=1000,initDepth=1,shape='FUNCTIONAL',param1=10000,param2=0,param3=1000000000):
+        """
+        Creates a tank with a very very large area so that it won't change with outflow, as close as I can get to a true reservoir
+        initDepth needs to be bigger than 0 to ensure there is water in the res.
+        by default it takes the elevation to be EPANET.head - 1 with initial Depth = 1.
+        :param maxDepth:
+        :param initDepth:
+        :param shape:
+        :param param1:
+        :param param2:
+        :param param3:
+        :return:
+        """
+        res = [str(self.id),str(float(self.head)-initDepth),str(maxDepth),str(initDepth),str(shape),str(param1),str(param2),str(param3)]
+        return res
 
 class Tank:
     def __init__(self, id, elevation, initLevel, minLevel, maxLevel, diameter, minVol, volCurve = None):
@@ -19,6 +88,12 @@ class Tank:
         self.diameter = diameter
         self.minVol = minVol
         self.volCurve = volCurve
+        self.area = 3.1415 * float(self.diameter)**2 / 4.
+
+    def SWMM_writer(self, shape='FUNCTIONAL', param1=0, param2=0):
+        tank = [str(self.id), str(self.elev), str(self.maxLevel), str(self.initLevel), str(shape), str(param1), str(param2),
+               str(self.area)]
+        return tank
 
 class Pipe:
     def __init__(self, id, node1, node2, length, diameter, roughness, minorLoss, status):
@@ -26,17 +101,25 @@ class Pipe:
         self.node1 = node1
         self.node2 = node2
         self.length = length
-        self.diameter = diameter
+        self.diameter = float(diameter)/1000.
         self.roughness = roughness
         self.minorLoss = minorLoss
         self.status = status
 
+    def SWMM_writer(self,inHeight = 0,outHeight=0,initFlow=0,Geom2=0,Geom3=0,Geom4=0,Barrels=1):
+        conduit = [str(self.id),str(self.node1),str(self.node2),str(self.length),str(self.roughness),str(inHeight),str(outHeight),str(initFlow)]
+        section = [str(self.id),'CIRCULAR',str(self.diameter),str(Geom2),str(Geom3),str(Geom4),str(Barrels)]
+        return conduit, section
 class Pump:
     def __init__(self, id, node1, node2, parameter):
         self.id = id
         self.node1 = node1
         self.node2 = node2
         self.parameter = parameter
+
+    def SWMM_writer(self,curve = '',initStatus='ON'):
+        pump = [str(self.id),str(self.node1),str(self.node2),str(curve),str(initStatus)]
+        return pump
 
 class Valve:
     def __init__(self, id, node1, node2, diameter, type, setting, minorLoss):
@@ -48,7 +131,9 @@ class Valve:
         self.setting = setting
         self.minorLoss = minorLoss
 
-
+    def SWMM_writer(self,type = 'SIDE',offset = 0,Qcoeff=0.65,gated='NO',closeTime=0):
+        valve = [str(self.id),str(self.node1),str(self.node2),str(type),str(offset),str(Qcoeff),str(gated),str(closeTime)]
+        return valve
 class Tag:
     def __init__(self):
         self.id
@@ -60,11 +145,23 @@ class Demand:
         self.pattern = pattern
         self.category = category
 
+    def SWMM_writer(self,Constit='FLOW',timeSeries='""',pattType='FLOW',mFactor=1.0,sFactor=1.0):
+        demand = [str(self.junction),str(Constit),str(timeSeries),str(pattType),str(mFactor),str(sFactor),str(-float(self.demand)),str(self.pattern)]
+        return demand
 class Pattern:
-    def __init__(self,id,multipliers):
+    def __init__(self,id,multipliers,firstInstance=True):
         self.id = id
         self.multipliers = multipliers
+        self.firstInstance = firstInstance
 
+    def SWMM_writer(self,Type = 'HOURLY'):
+        demand = [str(self.id)]
+        if self.firstInstance == True:
+            demand.append(str(Type))
+        for multi in self.multipliers:
+            demand.append(str(multi))
+
+        return demand
 class Curve:
     def __init__(self, id, X,Y):
         self.id= id
@@ -72,7 +169,7 @@ class Curve:
         self.y_Value = Y
 
 class Control:
-    def __init(self,rule):
+    def __init__(self,rule):
         self.rule = rule
 
 class Emitter:
@@ -85,20 +182,36 @@ class Coordinate:
         self.node = node
         self.x_Coord = X
         self.y_Coord = Y
-
+    def SWMM_writer(self):
+        coord = [str(self.node),str(self.x_Coord),str(self.y_Coord)]
+        return coord
 class Vertice:
     def __init__(self,link, X,Y):
         self.link = link
         self.x_Coord = X
         self.y_Coord = Y
 
+    def SWMM_writer(self):
+        coord = [str(self.link), str(self.x_Coord), str(self.y_Coord)]
+        return coord
+
 class Label:
     def __init__(self, X, Y, label):
         self.x_Coord = X
         self.y_Coord = Y
-        self.lable = label
+        self.label = label
+
+    def SWMM_writer(self):
+        label = [str(self.x_Coord),str(self.y_Coord),str(self.label)]
+        return label
 
 class Options:
     def __init__(self,option, value):
         self.option = option
         self.value = value
+
+    def SWMM_writer(self):
+        if self.option == 'Units':
+            self.option = 'FLOW_UNITS'
+        option_string = str(self.option)+'\t\t\t'+str(self.value)+'\n'
+        return option_string
--- pyEPANETtoSWMM.py
+++ pyEPANETtoSWMM.py
@@ -2,30 +2,24 @@
 #import pylab as pp
 #from epanettools import epanet2 as epa
 import inpClasses as inp
+import os
+import sys
 
-inp_filename = 'Example_Networks/Net1.inp'
+
+
+#inp_filename = 'Example_Networks/CW.inp'
+#SWMM_filename = 'Example_Networks/CW_SWMM.inp'
 
 #### Creating the list of objects
-Juncs = []
-Reservoirs = []
-Tanks = []
-Pipes = []
-Pumps = []
-Valves = []
-Tags = []
-Demands = []
-Patterns = []
-Curves = []
-Controls = []
-Emitters = []
-Coordinates = []
-Vertices = []
-Labels = []
-Option = []
 
+inp_filename = sys.argv[1]
+SWMM_filename = sys.argv[2]
 
 inp_file = open(inp_filename,'r')
 section = None          #Which section of the EPANET inp file are we currently reading
+Net = 0
+Net = inp.Network(os.path.splitext(os.path.basename(inp_filename))[0])
+CurrentPattern = None
 for line in inp_file:
     if line.startswith(';') or line.strip() == "":
         continue
@@ -33,20 +27,193 @@ for line in inp_file:
         section = line.strip()
     else:
         vals = line.split()
-        if section == '[JUNCTIONS]':
-            Juncs.append(inp.Junc(vals[0],vals[1],vals[2]))
+        if section == '[TITLE]':
+            Net.titles.append(inp.Title(line))
+
+        elif section == '[JUNCTIONS]':
+            try:
+                Net.juncs.append(inp.Junc(vals[0], vals[1], vals[2],pattern=vals[3]))
+            except:
+                Net.juncs.append(inp.Junc(vals[0],vals[1],vals[2]))
 
         elif section == '[RESERVOIRS]':
-            Reservoirs.append(inp.Reservoir(vals[0],vals[1]))
+            Net.reservoirs.append(inp.Reservoir(vals[0],vals[1]))
 
         elif section == '[TANKS]':
-            Tanks.append(inp.Tank(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6]))
+            Net.tanks.append(inp.Tank(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6]))
 
         elif section == '[PIPES]':
-            Pipes.append(inp.Pipe(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7]))
+            Net.pipes.append(inp.Pipe(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6],vals[7]))
 
         elif section == '[PUMPS]':
-            Pumps.append(inp.Pump(vals[0],vals[1],vals[2],vals[3]))
+            Net.pumps.append(inp.Pump(vals[0],vals[1],vals[2],vals[3]))
 
         elif section == '[VALVES]':
-            Valves.append(inp.Valve(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6]))
+            Net.valves.append(inp.Valve(vals[0],vals[1],vals[2],vals[3],vals[4],vals[5],vals[6]))
+
+        elif section == '[TAGS]':
+            Net.tags.append(inp.Tag(vals[0]))
+
+        elif section == '[DEMANDS]':
+            Net.demands.append(inp.Demand(vals[0],vals[1],vals[2],vals[3]))
+
+        elif section == '[PATTERNS]':
+            #print(vals[0],vals[1:])
+            Name = vals[0]
+            if Name == CurrentPattern:
+                firstInstance = False
+            else:
+                firstInstance = True
+            CurrentPattern = Name
+            Net.patterns.append(inp.Pattern(vals[0],vals[1:],firstInstance))
+
+        elif section == '[CURVES]':
+            Net.curves.append(inp.Curve(vals[0],vals[1],vals[2]))
+
+        elif section == '[CONTROLS]':
+            Net.controls.append(inp.Control(line))
+
+        elif section == '[EMITTERS]':
+            Net.emitters.append(inp.Emitter(vals[0],vals[1]))
+
+        elif section == '[COORDINATES]':
+            Net.coordinates.append(inp.Coordinate(vals[0],vals[1],vals[2]))
+
+        elif section == '[LABELS]':
+            Net.labels.append(inp.Label(vals[0],vals[1],vals[2]))
+
+        elif section == '[OPTIONS]':
+            Net.options.append(inp.Options(vals[0],vals[1]))
+
+        elif section == '[VERTICES]':
+            Net.vertices.append(inp.Vertice(vals[0],vals[1],vals[2]))
+inp_file.close()
+
+out_file = open(SWMM_filename,'w')
+####  Title
+out_file.write('[TITLE]\n')
+out_file.write(';;Project Title/Notes\n')
+for line in Net.titles:
+    out_file.write(line.text)
+out_file.write('Converted using pyEPANETtoSWMM\n')
+out_file.write('\n')
+
+#### Junctions
+out_file.write('[JUNCTIONS]\n')
+out_file.write(';;Name\t\t\tElevation\t\t\tMaxDepth\t\t\tInitDepth\t\t\tSurDepth\t\t\tAponded\n')
+for junc in Net.juncs:
+    out_file.write('\t\t\t'.join(junc.SWMM_writer())+'\n')
+out_file.write('\n')
+
+#### Inflows / Demands
+out_file.write('[INFLOWS]\n')
+out_file.write(';;Node           Constituent      Time Series      Type     Mfactor  Sfactor  Baseline Pattern\n')
+for demand in Net.demands:
+    out_file.write('\t\t\t'.join(demand.SWMM_writer())+'\n')
+for junction in Net.juncs:
+    if float(junction.demand) != 0:
+        out_file.write('\t\t\t'.join(junction.SWMM_demand_writer()) + '\n')
+out_file.write('\n')
+
+#### Storage
+out_file.write('[STORAGE]\n')
+out_file.write(';;Name           Elev.    MaxDepth   InitDepth  Shape      Curve Name/Params            N/A      Fevap    Psi      Ksat     IMD   \n')
+for reservoir in Net.reservoirs:
+    out_file.write('\t\t\t'.join(reservoir.SWMM_writer()) + '\n')
+for tank in Net.tanks:
+    out_file.write('\t\t\t'.join(tank.SWMM_writer()) + '\n')
+out_file.write('\n')
+
+#### Conduits
+out_file.write('[CONDUITS]\n')
+out_file.write(';;Name           From Node        To Node          Length     Roughness  InOffset   OutOffset  InitFlow   MaxFlow  \n')
+Sections = []
+for pipe in Net.pipes:
+    conduit,section = pipe.SWMM_writer()
+    out_file.write('\t\t\t'.join(conduit)+'\n')
+    Sections.append(section)
+out_file.write('\n')
+
+#### Sections
+out_file.write('[XSECTIONS]\n')
+out_file.write(';;Link           Shape        Geom1            Geom2      Geom3      Geom4      Barrels    Culvert \n')
+for section in Sections:
+    out_file.write('\t\t\t'.join(section)+'\n')
+out_file.write('\n')
+
+#### Pumps
+out_file.write('[PUMPS]\n')
+out_file.write(';;Name             InNode             OutNode             Curve            initStatus \n')
+for pump in Net.pumps:
+    out_file.write('\t\t\t'.join(pump.SWMM_writer())+'\n')
+out_file.write('\n')
+
+
+#### Coordinates
+out_file.write('[COORDINATES]\n')
+out_file.write(';;Node           X-Coord            Y-Coord \n')
+for coord in Net.coordinates:
+    out_file.write('\t\t\t'.join(coord.SWMM_writer()) + '\n')
+out_file.write('Dummy\t\t\t0\t\t\t0')
+out_file.write('\n')
+
+#### Outfalls
+out_file.write('[OUTFALLS]\n')
+out_file.write(';;Name           Elevation  Type       Stage Data       Gated    Route To         \n')
+out_file.write('Dummy                0          FREE                        NO                      \n')
+out_file.write('\n')
+
+#### Labels
+out_file.write('[LABELS]\n')
+out_file.write(';;X-Coord         	Y-Coord           	Label \n')
+for label in Net.labels:
+    out_file.write('\t\t\t'.join(label.SWMM_writer())+ '\n')
+out_file.write('\n')
+
+#### Vertices
+out_file.write('[VERTICES]\n')
+out_file.write(';;Link             X-Coord          Y-Coord \n')
+for vertex in Net.vertices:
+    out_file.write('\t\t\t'.join(vertex.SWMM_writer())+ '\n')
+out_file.write('\n')
+
+#### Orifices
+out_file.write('[ORIFICES]\n')
+out_file.write(';;Name           From Node        To Node          Type         Offset     Qcoeff     Gated    CloseTime \n')
+for valve in Net.valves:
+    out_file.write('\t\t\t'.join(valve.SWMM_writer())+ '\n')
+out_file.write('\n')
+
+
+
+#### Patterns
+out_file.write('[PATTERNS]\n')
+out_file.write(';;Name             Type       Multipliers\n')
+for pattern in Net.patterns:
+    out_file.write('\t\t\t'.join(pattern.SWMM_writer())+'\n')
+out_file.write('\n')
+
+#### Options
+out_file.write('[OPTIONS]\n')
+out_file.write('FLOW_ROUTING         DYNWAVE\n')
+out_file.write('\n')
+
+
+out_file.write('START_DATE\t\t\t01/01/2020\n')
+out_file.write('START_TIME           00:00:00\n')
+out_file.write('REPORT_START_DATE\t\t\t01/01/2020\n')
+out_file.write('REPORT_START_TIME    00:00:00\n')
+out_file.write('END_DATE             01/01/2020\n')
+out_file.write('END_TIME             06:00:00\n')
+
+
+
+
+
+
+
+
+
+
+
+out_file.close()
